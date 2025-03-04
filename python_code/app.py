2e1ac639d833f0af2bdb5d020d4f30299e6aa63d
sandyyy741821 <santhoshsandyyy741821@gmail.com>
2025-03-04 16:54:03 +0530

    updated file

--- python_code/app.py
+++ python_code/app.py
@@ -1,23 +1,6 @@
 """
 Message object checks
 """
-import pandas as pd
-
-def myFunction():  
-    """
-    continue
-    """
-    pass
-
-def calculate_sum(a, b):
-    """
-    calc
-    """
-    if a > b:
-        return a + b
-    else:
-        return a - b
-z=x+10
 def greet(name):
     """
     greeting message
