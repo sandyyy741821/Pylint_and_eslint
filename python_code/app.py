bb1a4330b517b31e5107cba196db69d6533d0867
sandyyy741821 <santhoshsandyyy741821@gmail.com>
2025-03-04 16:40:56 +0530

    Save changes before switching branches

--- python_code/app.py
+++ python_code/app.py
@@ -1,8 +1,26 @@
+"""
+Message object checks
+"""
+import pandas as pd
 
+def myFunction():  
+    """
+    continue
+    """
+    pass
 
+def calculate_sum(a, b):
+    """
+    calc
+    """
+    if a > b:
+        return a + b
+    else:
+        return a - b
+z=x+10
 def greet(name):
-
-    print(f"Hello, {name}!")
-
-
+    """
+    greeting message
+    """
+    print(f'Hello,{name}!')
 greet("World")
